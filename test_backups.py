8ab52df290f813c6d084f6580cac1aa1e3096909
hamed <hamediran1999@gmail.com>
2022-04-25 17:52:41 +0430

    add test for all function

--- test_backups.py
+++ test_backups.py
@@ -1,7 +1,9 @@
+from pickle import NONE
 from mysql.backups import MySql
 from os import getenv
 from dotenv import load_dotenv
 import docker
+import warnings
 
 load_dotenv()
 db_password = getenv('db_password')
@@ -9,21 +11,26 @@ db_host= getenv('db_host')
 db_host_port = getenv('db_host_port')
 db_user = getenv('db_user')
 db_type= getenv('db_type')
-container_name= getenv('container_name')
+try:
+    container_name= getenv('container_name')
+except:
+    raise("you use none contiber database")
+
+db = MySql(db_password, db_host, db_host_port, db_user, db_type)
 
 def db_connect():
-    db = MySql(db_password, db_host, db_host_port, db_user, db_type)
+    
     db_enggine = db.config_engine()
     try:
         with db_enggine.connect() as connection:
             return True
     except:
         return False
-    
+
 def db_continer():
     client = docker.from_env()
     try:
-        container = client.containers.get(container_name)
+        client.containers.get(container_name)
         return True
     except:
         return False
@@ -31,9 +38,14 @@ def db_continer():
     
 def test_dabasa_connect():
     assert db_connect() == True
-
-def test_config_continer():
-    assert db_continer() == True
+if container_name:
+    def test_config_continer():
+        assert db_continer() == True
 
 def test_get_mysql_database_list():
-    pass
+    db_list = db.get_mysql_database_list()
+    assert "sys" in db_list
+    
+def test_mysql_backup_command():
+    assert (db.mysql_backup_command("sys"),None)
+        
